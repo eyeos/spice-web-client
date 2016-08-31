d95186410b8a4b718f74cf9c6e889902b1b9ca65
Malte Starostik <github@tsoqxod.de>
2016-08-31 14:23:49 +0200

    Added license header

--- keymaps/keymapde.js
+++ keymaps/keymapde.js
@@ -1,3 +1,22 @@
+/*
+ eyeOS Spice Web Client
+ Copyright (c) 2015 M2R
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Affero General Public License version 3 as published by the
+ Free Software Foundation.
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ details.
+ 
+ You should have received a copy of the GNU Affero General Public License
+ version 3 along with this program in the file "LICENSE".  If not, see 
+ <http://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
+
+// These tables map the js keyboard keys to the spice equivalent
+
 wdi.KeymapDE = function() {
 
     var charmapDE = [];
